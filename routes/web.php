cc0a4f382f0764f58123bf58f9e346de14be29bd
MS <mo7ammed.sabre@gmail.com>
2026-09-13 16:19:24 +0300

    Setup Laravel POS authentication with Arabic RTL views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/{page}', [AdminController::class, 'index']);
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
